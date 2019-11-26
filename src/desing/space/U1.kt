0c1d607667b2aebc7cf72a6ea492a3b0268a1c21
RR <Roberto_Rubio98@outlook.com>
2019-11-26 13:50:13 -0600

    Agregado el override de las funciones launch y land a la clase U1

--- src/desing/space/U1.kt
+++ src/desing/space/U1.kt
@@ -1,5 +1,12 @@
 package desing.space
 
-class U1() {
+class U1:Rocket() {
+    override fun launch(): Boolean {
+        return true
+    }
+
+    override fun land(): Boolean {
+        return true
+    }
 
 }
