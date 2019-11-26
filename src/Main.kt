be85317d5a41a371b1c759035c333ec8dbf83b59
RR <Roberto_Rubio98@outlook.com>
2019-11-26 17:07:11 -0600

    Cambio en el tipo retorno de carry

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,19 @@
-import Simulation.Simulation
-import desing.space.U1
 import desing.supplies.Items
 import java.io.File
 
 fun main(){
-
-    //val cadena = phase1.useLines { readLine() }
-    //print(cadena)
+    var aux: Array<String>
+    val itemsList = ArrayList<Items>()
+    val pathAux: String = System.getProperty("user.dir").toString()
+    File("$pathAux\\src\\Simulation\\Phases\\Phase-1.txt").forEachLine {
+        aux = it.split("=").toTypedArray()
+        itemsList.add(Items(aux[0],aux[1].toInt()))
+    }
+    File("$pathAux\\src\\Simulation\\Phases\\Phase-2.txt").forEachLine {
+        aux = it.split("=").toTypedArray()
+        itemsList.add(Items(aux[0],aux[1].toInt()))
+    }
+    for(items in itemsList){
+        println(items.status)
+    }
 }
--- src/desing/space/SpaceShip.kt
+++ src/desing/space/SpaceShip.kt
@@ -6,5 +6,5 @@ interface SpaceShip {
     fun launch() :Boolean
     fun land(): Boolean
     fun canCarry(Item: Items):Boolean//Item = Item to add to carry capacity, Capacity left to the rocket
-    fun carry(Item: Items): Int?//Loads the item to the cargo ship
+    fun carry(Item: Items)//Loads the item to the cargo ship
 }
