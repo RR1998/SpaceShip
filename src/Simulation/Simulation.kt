8a12a949b4d18c3ba574eb7f15282c6f10833525
RR <Roberto_Rubio98@outlook.com>
2019-11-26 16:33:23 -0600

    Implementacion del metodo "loadItems"

--- src/Simulation/Simulation.kt
+++ src/Simulation/Simulation.kt
@@ -1,5 +1,21 @@
 package Simulation
 
-class Simulation {
+import desing.supplies.Items
+import java.io.BufferedReader
+import java.io.File
 
+class Simulation {
+    fun loadItems(){
+        var aux: Array<String>
+        val itemsList = ArrayList<Items>()
+        val PathAux: String = System.getProperty("user.dir").toString()
+        File("$PathAux\\src\\src\\Simulation\\Phases\\Phase-1.txt").forEachLine {
+            aux = it.split("=").toTypedArray()
+            itemsList.add(Items(aux[0],aux[1].toInt()))
+        }
+        File("$PathAux\\src\\Simulation\\Phases\\Phase-2.txt").forEachLine {
+            aux = it.split("=").toTypedArray()
+            itemsList.add(Items(aux[0],aux[1].toInt()))
+        }
+    }
 }
