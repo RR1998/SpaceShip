0e75ae9fa3b52f3abb0cdf4cd2b1f19c97d83ee3
RR <Roberto_Rubio98@outlook.com>
2019-11-26 14:32:27 -0600

    Implementacion del metodo, launch, land y successOrFailure

--- src/desing/space/U1.kt
+++ src/desing/space/U1.kt
@@ -1,12 +1,16 @@
 package desing.space
 
+import kotlin.random.Random
+
 class U1:Rocket() {
     override fun launch(): Boolean {
-        return true
+        return successOrFailure()
     }
-
     override fun land(): Boolean {
-        return true
+        return successOrFailure()
     }
-
+    private fun successOrFailure():Boolean{
+        val launchedRocket: Double = Random.nextDouble(0.00,1.00)
+        return chanceLaunchExplosion < launchedRocket
+    }
 }
