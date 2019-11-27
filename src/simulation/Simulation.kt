31fd22d6eb8979b3fddb0ba15c207a3cbb77f7bb
RR <Roberto_Rubio98@outlook.com>
2019-11-26 18:51:55 -0600

    Ajuste estetico a la clase Simulation

--- src/simulation/Simulation.kt
+++ src/simulation/Simulation.kt
@@ -1,30 +1,25 @@
 package simulation
-
-import desing.space.*
+import desing.space.Rocket
+import desing.space.U1
+import desing.space.U2
 import desing.supplies.Items
 import java.io.File
-
-class Simulation() {
+class Simulation {
     init {
 
     }
-    private fun loadItems(): ArrayList<Items> {
+    private fun loadItems(numberPhase: Int): ArrayList<Items> {
         var aux: Array<String>
         val itemsList = ArrayList<Items>()
         val pathAux: String = System.getProperty("user.dir").toString()
-        File("$pathAux\\src\\Simulation\\Phases\\Phase-1.txt").forEachLine {
-            aux = it.split("=").toTypedArray()
-            itemsList.add(Items(aux[0], aux[1].toInt()))
-        }
-        File("$pathAux\\src\\Simulation\\Phases\\Phase-2.txt").forEachLine {
+        File("$pathAux\\src\\Simulation\\Phases\\Phase-$numberPhase.txt").forEachLine {
             aux = it.split("=").toTypedArray()
             itemsList.add(Items(aux[0], aux[1].toInt()))
         }
         return itemsList
     }
-
-    fun loadU1(): ArrayList<U1>{
-        val itemsList = loadItems()
+    fun loadU1(): ArrayList<U1> {
+        val itemsList = loadItems(1)
         val rocketList: ArrayList<U1> = ArrayList()
         var flag = true
         var count = 0
@@ -32,7 +27,8 @@ class Simulation() {
             val r1 = U1()//Rocket variable
             r1.createRocket()//Generate a rocket
             println(r1.rocketWeight)
-            itemsList.forEach { item ->//For that fills the rockets
+            itemsList.forEach { item ->
+                //For that fills the rockets
                 if (r1.canCarry(item) && item.status) {
                     item.status = false
                     r1.carry(item)
@@ -48,8 +44,9 @@ class Simulation() {
         }
         return rocketList
     }
-    fun loadU2(): ArrayList<U2>{
-        val itemsList = loadItems()
+
+    fun loadU2(): ArrayList<U2> {
+        val itemsList = loadItems(2)
         val rocketList: ArrayList<U2> = ArrayList()
         var flag = true
         var count = 0
@@ -57,7 +54,8 @@ class Simulation() {
             val r2 = U2()//Rocket variable
             r2.createRocket()//Generate a rocket
             println(r2.rocketWeight)
-            itemsList.forEach { item ->//For that fills the rockets
+            itemsList.forEach { item ->
+                //For that fills the rockets
                 if (r2.canCarry(item) && item.status) {
                     item.status = false
                     r2.carry(item)
@@ -72,5 +70,9 @@ class Simulation() {
             }
         }
         return rocketList
+    }
+    fun runSimulation(ArrayRockets: ArrayList<Rocket>): Int{
+
+        return 0
     }
 }
