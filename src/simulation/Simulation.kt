e38e00b86e4f8716a51c538fcd5ab961bb25a2bb
RR <Roberto_Rubio98@outlook.com>
2019-11-27 10:37:45 -0600

    Code cleanup

--- src/simulation/Simulation.kt
+++ src/simulation/Simulation.kt
@@ -1,11 +1,15 @@
 package simulation
+
 import desing.space.Rocket
 import desing.space.U1
 import desing.space.U2
 import desing.supplies.Items
 import java.io.File
+
 class Simulation {
-    init {}
+    init {
+    }
+
     private fun loadItems(numberPhase: Int): ArrayList<Items> {
         var aux: Array<String>
         val itemsList = ArrayList<Items>()
@@ -16,6 +20,7 @@ class Simulation {
         }
         return itemsList
     }
+
     fun loadU1(phase: Int): ArrayList<Rocket> {
         val itemsList = loadItems(phase)
         val rocketList: ArrayList<Rocket> = ArrayList()
@@ -34,10 +39,13 @@ class Simulation {
             }
             r1.crashExplosionChanceSelection(r1.rocketModel, r1.rocketWeight, r1.maxWeight)
             rocketList.add(r1)
-            if (count == itemsList.size) { flag = false }
+            if (count == itemsList.size) {
+                flag = false
+            }
         }
         return rocketList
     }
+
     fun loadU2(phase: Int): ArrayList<Rocket> {
         val itemsList = loadItems(phase)
         val rocketList: ArrayList<Rocket> = ArrayList()
@@ -56,23 +64,25 @@ class Simulation {
             }
             r2.crashExplosionChanceSelection(r2.rocketModel, r2.rocketWeight, r2.maxWeight)
             rocketList.add(r2)
-            if (count == itemsList.size) { flag = false }
+            if (count == itemsList.size) {
+                flag = false
+            }
         }
         return rocketList
     }
-    fun runSimulation(ArrayRockets: ArrayList<Rocket>): Int{
-        var totalBudget = 0
+
+    fun runSimulation(ArrayRockets: ArrayList<Rocket>): Int {
+        val totalBudget: Int
         var count = 0
-        var rocketCost: Int = ArrayRockets[0].rocketCost
-        ArrayRockets.forEach{RocketLaunched ->
-            //totalBudget += RocketLaunched.rocketCost
-            while(!RocketLaunched.launch() && !RocketLaunched.land()){
+        val rocketCost: Int = ArrayRockets[0].rocketCost
+        ArrayRockets.forEach { RocketLaunched ->
+            while (!RocketLaunched.launch() && !RocketLaunched.land()) {
                 count++
             }
             count++
             println("launched $count")
         }
-        totalBudget =  rocketCost * count
+        totalBudget = rocketCost * count
         return totalBudget
     }
 }
