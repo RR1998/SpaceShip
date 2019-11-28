1311d30d242e87a5bd593dcf9143f846e53448b6
RR <Roberto_Rubio98@outlook.com>
2019-11-27 18:12:08 -0600

    Format code

--- src/Main.kt
+++ src/Main.kt
@@ -2,25 +2,25 @@ import desing.space.Rocket
 import simulation.Simulation
 
 fun main() {
-    for (l in 1..100){
-    val simulationVariable = Simulation()
-    var rocketList: ArrayList<Rocket>
-    var budget: Int
-    for (i in 1..2) {
-        val rocketType = "U-$i"
-        val loadingString = "Loading fleet of $rocketType ships"
-        println(loadingString)
-        if (rocketType == "U-1") {
-            rocketList = simulationVariable.loadU1()
-            budget = simulationVariable.runSimulation(rocketList)
-            loadProcess(rocketList, budget)
+    for (l in 1..100) {
+        val simulationVariable = Simulation()
+        var rocketList: ArrayList<Rocket>
+        var budget: Int
+        for (i in 1..2) {
+            val rocketType = "U-$i"
+            val loadingString = "Loading fleet of $rocketType ships"
+            println(loadingString)
+            if (rocketType == "U-1") {
+                rocketList = simulationVariable.loadU1()
+                budget = simulationVariable.runSimulation(rocketList)
+                loadProcess(rocketList, budget)
+            }
+            if (rocketType == "U-2") {
+                rocketList = simulationVariable.loadU2()
+                budget = simulationVariable.runSimulation(rocketList)
+                loadProcess(rocketList, budget)
+            }
         }
-        if (rocketType == "U-2") {
-            rocketList = simulationVariable.loadU2()
-            budget = simulationVariable.runSimulation(rocketList)
-            loadProcess(rocketList, budget)
-        }
-    }
     }
 }
 
